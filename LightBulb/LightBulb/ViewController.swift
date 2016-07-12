9c67a09e9806f0a77a7c6f9740f2e87b4eeaf9fd
rebeccabartels <rebeccakbartels@icloud.com>
2016-07-12 14:47:36 -0400

    BEAUTIFUL

--- LightBulb/LightBulb/ViewController.swift
+++ LightBulb/LightBulb/ViewController.swift
@@ -9,18 +9,49 @@
 import UIKit
 
 class ViewController: UIViewController {
-    @IBOutlet weak var lightBulb: UIView!
+
+    @IBOutlet weak var lightBulb: UIImageView!
+    @IBOutlet weak var segmentControl: UISegmentedControl!
+
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // TODO: Change background color of lightBulb view to red
+        self.view.backgroundColor = UIColor.purpleColor()
+        self.lightBulb.backgroundColor = UIColor.redColor()
+        
+        segmentControl.layer.cornerRadius = 5.0  // Doesn't let the background bleed
+        segmentControl.backgroundColor = UIColor.blackColor()
+        segmentControl.tintColor = UIColor.whiteColor()
+        //Changed background color of lightBulb view to red, view bg to purps, and set the radius and tint color and background color of the segment controller.
     }
 
     func changeColor(to color: UIColor) {
-        // TODO: Change background color to "color"
+        
+        
+    
     }
 
-    @IBAction func colorSelected(sender: UISegmentedControl) {
-        // TODO: Change background color when segmented control changes
+    @IBAction func colorSelected(sender: UISegmentedControl)
+    {
+        print(segmentControl.selectedSegmentIndex)
+        
+        if (segmentControl.selectedSegmentIndex == 0)
+        {
+            lightBulb.backgroundColor = UIColor.redColor()
+        }
+        else if(segmentControl.selectedSegmentIndex == 1)
+        {
+            lightBulb.backgroundColor = UIColor.yellowColor()
+        }
+        else if(segmentControl.selectedSegmentIndex == 2)
+        {
+            lightBulb.backgroundColor = UIColor.blueColor()
+        }
+        else if(segmentControl.selectedSegmentIndex == 3)
+        {
+            lightBulb.backgroundColor = UIColor.greenColor()
+        }
+
     }
 }
